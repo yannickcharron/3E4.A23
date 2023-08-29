cd2efb5451af5b61f8c56f5f253c707b8ab0ec84
Yannick Charron <ycharron@cstj.qc.ca>
2023-08-29 10:47:04 -0400

    Début de ma structure de projet

---
+++ S02.REST/server.js
@@ -0,0 +1,5 @@
+//const chalk = require('chalk'); Ancienne méthode d'import de bibliothèque
+//server.js => fichier de démarrage => équivalent au main
+import chalk from 'chalk';
+
+console.log(chalk.blue.bgGreenBright.bold('😍 Bonjour en couleur'));
