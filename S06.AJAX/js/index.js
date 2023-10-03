4140a06904e500a1a908b5582d9d54edc40cc049
Yannick Charron <ycharron@cstj.qc.ca>
2023-10-03 10:33:08 -0400

    feature: Card de la planète avec le nom

--- S06.AJAX/js/index.js
+++ S06.AJAX/js/index.js
@@ -9,8 +9,26 @@ $(document).ready(() => {
 async function retrieveAllPlanet() {
     try {
         const response = await axios.get(SERVICE_URL);
-        console.log(response);
+        if(response.status === 200) {
+            //OK
+            const planets = response.data;
+            displayPlanets(planets);
+        }
     } catch(err) {
         console.log(err);
     }
+}
+
+function displayPlanets(planets) {
+    planets.forEach(p => {
+        $('#planets').append(displayPlanet(p));
+    }); 
+}
+
+function displayPlanet(planet) {
+    let htmlPlanet = '<div class="col-2 card m-2">';
+    htmlPlanet += `<h5 class="card-title">${planet.name}</h5>`;
+    htmlPlanet += '</div>';
+
+    return htmlPlanet;
 }
