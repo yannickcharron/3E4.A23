f29a6d2f1a6fc75979c7ca17ec8822cb610c194d
Yannick Charron <ycharron@cstj.qc.ca>
2023-10-20 15:11:11 -0400

    feature: extraction - ajout portal

--- S06.AJAX/js/details.js
+++ S06.AJAX/js/details.js
@@ -1,3 +1,5 @@
+const ELEMENT_IMG_URL = 'https://assets.andromia.science/elements';
+
 const urlParams = {};
 (window.onpopstate = function () {
   let match;
@@ -14,7 +16,67 @@ const urlParams = {};
 $(document).ready(() => {
   const hrefPlanet = urlParams.planet; //.planet est le nom du paramètre que j'ai choisi dans index.js
   retrievePlanet(hrefPlanet);
+
+  //Click sur le bouton ajouter un portal
+  $('#btnAddPortal').click(() => {
+    addPortal();
+  });
+
+  $('#btnMiner').click(() => {
+    minerPlanet();
+  });
+
 });
+
+async function minerPlanet() {
+
+  const url = `${urlParams.planet}/actions?type=mine`;
+
+  try {
+    const response = await axios.get(url);
+    if(response.status === 200) {
+      const elements = response.data;
+      $('#extraction tbody').empty();
+      elements.forEach(e => {
+        const elementHtml = `<tr><td><img class="imgElement" src="${ELEMENT_IMG_URL}/${e.element}.png"></td><td>${e.quantity}</td></tr>`;
+        $('#extraction').append(elementHtml);
+      });
+
+    }
+ 
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+async function addPortal() {
+
+  const position = $('#txtPosition').val();
+  const affinity = $('#cboAffinity').val();
+
+  const body = {
+    position: position,
+    affinity: affinity
+  }
+  console.log(body);
+
+  try {
+    const url = `${urlParams.planet}/portals`
+    const response = await axios.post(url, body);
+
+    if(response.status === 201) {
+      const portal = response.data;
+      const portalHtml = `<tr><td>${portal.position}</td><td><img class="imgAffinityPortal" src="img/${portal.affinity}.svg" alt="${portal.affinity}" /></td></tr>`;
+      $("#portals").prepend(portalHtml);
+    } else {
+      console.log(response);
+    }
+
+  } catch(err) {
+    console.log(err);
+  }
+
+}
 
 async function retrievePlanet(href) {
   try {
